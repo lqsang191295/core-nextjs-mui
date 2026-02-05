79d9d91b2e9a20ce049b60922a5b8115b61b5524
sangledev95 <lqsang19952@gmail.com>
2026-02-05 16:32:50 +0700

    first commit

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,15 +1,28 @@
 "use client";
-import { Box } from "@mui/material";
+
+import { useSidebarStore } from "@/store/useSidebarStore";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 import Sidebar from "./Sidebar";
 
+const drawerWidth = 240;
+
 export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const { open } = useSidebarStore();
+
   return (
-    <Box className="w-full h-full" sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex" }}>
       <Sidebar />
+
       <Box
         component="main"
-        sx={{ p: 0, height: "100vh", width: "100vw", overflow: "hidden" }}
-        className="flex-1">
+        sx={{
+          flexGrow: 1,
+          ml: !isMobile && open ? `${0}px` : 0,
+          transition: "margin 0.25s",
+          height: "100vh",
+        }}>
         {children}
       </Box>
     </Box>
